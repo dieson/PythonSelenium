4b3311d44a75e8b19f527af0cb72e6c57029b775
Dieson Zuo <Dieson@DiesondeMacBook-Pro.local>
2018-05-29 18:14:21 +0800

    update the chromedriver path

--- Driver/Driver.py
+++ Driver/Driver.py
@@ -5,14 +5,18 @@
 import os
 from selenium import webdriver
 from Utils.Utils import Utils
+import platform
 
 
 class Driver(object):
     def __init__(self):
         self.wait_time = Utils.get_conf("script", "wait_time")
         self.url = Utils.get_conf("serverconf", "server_url")
-        self.chromeDriver = os.path.join(Utils.get_project_path(), "Resources", "chromedriver.exe")
-        self.driver = webdriver.Chrome(self.chromeDriver)
+        if "Win" in platform.system():
+            self.chromedriver = os.path.join(Utils.get_project_path(), "Resources", "chromedriver.exe")
+        else:
+            self.chromedriver = os.path.join(Utils.get_project_path(), "Resources", "chromedriver")
+        self.driver = webdriver.Chrome(self.chromedriver)
         self.driver.maximize_window()
         self.driver.get(self.url)
 
